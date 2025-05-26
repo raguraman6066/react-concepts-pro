3f735e8b0b59161dcb748ef609607b352356fd9a
raguraman6066 <raguraman.mps6066@gmail.com>
2025-05-26 18:23:34 +0530

    added todo

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CounterApp from "./CounterApp";
 import Shop from "./Shop";
+import { FaTrash } from "@react-icons/all-files/fa/FaTrash";
+import { FaEdit } from "@react-icons/all-files/fa/FaEdit";
+
 let Button = styled.button`
   background-color: blue;
   color: white;
@@ -28,7 +31,51 @@ const Content = () => {
     user = "demo";
     console.log(user);
   }
-
+  let [items, setItems] = useState([
+    { id: 1, label: "html&css", checked: true },
+    { id: 2, label: "javascript", checked: false },
+    { id: 3, label: "react", checked: true },
+  ]);
+  let [newItem, setNewItem] = useState("");
+  function handleDelete() {}
+  function handleChecked(id) {
+    let newListItems = items.map((item) => {
+      return item.id === id ? { ...item, checked: !item.checked } : item;
+    });
+    setItems(newListItems);
+  }
+  function handleSubmit() {
+    if (isEdit) {
+      let newListItem = items.map((item) => {
+        return item.id === currentId ? { ...item, label: newItem } : item;
+      });
+      setItems(newListItem);
+      setNewItem("");
+      setCurrentId(null);
+      setIsEdit(false);
+    } else {
+      setItems([
+        ...items,
+        { id: items.length + 1, label: newItem, checked: false },
+      ]);
+    }
+  }
+  let [isEdit, setIsEdit] = useState(false);
+  let [currentId, setCurrentId] = useState(null);
+  function handleDelete(id) {
+    let newList = items
+      .filter((item) => item.id !== id)
+      .map((item, index) => {
+        return { ...item, id: index + 1 };
+      });
+    setItems(newList);
+  }
+  function handleUpdate(id) {
+    let listItem = items.find((item) => item.id == id);
+    setNewItem(listItem.label);
+    setIsEdit(true);
+    setCurrentId(id);
+  }
   return (
     <main>
       {/* <h1 style={headingStyle}>Main Content - {user}</h1>
@@ -40,8 +87,40 @@ const Content = () => {
         Click Me
       </Button>
       <NewButton onClick={changeName}>New Button</NewButton> 
-      <CounterApp />*/}
-      <Shop />
+      <CounterApp />
+      <Shop />*/}
+      <div>
+        <input
+          type="text"
+          value={newItem}
+          placeholder="add new item"
+          onChange={(event) => setNewItem(event.target.value)}
+        />
+        <button onClick={handleSubmit}>{isEdit ? "Save" : "Add"}</button>
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id} className="item">
+            <input
+              type="checkbox"
+              value={item.checked}
+              checked={item.checked}
+              onChange={() => handleChecked(item.id)}
+            />
+            <label> {item.label}</label>
+            <FaEdit
+              onClick={() => handleUpdate(item.id)}
+              role="button"
+              tabIndex={0}
+            />
+            <FaTrash
+              onClick={() => handleDelete(item.id)}
+              role="button"
+              tabIndex={0}
+            />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
